bcf9447debbe1ad49d64fdefed006f6c7095fd4a
Vadim Macagon <vadim.macagon@gmail.com>
2015-08-11 22:46:17 +0700

    Remove typings for minimatch package

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="minimatch/minimatch.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="bluebird/bluebird.d.ts" />
 /// <reference path="mkdirp/mkdirp.d.ts" />
